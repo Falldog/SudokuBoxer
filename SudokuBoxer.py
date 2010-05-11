3cc156f56574bac34e6fccbec0174c03d13cb291
falldog <falldog@c8a44015-7bef-4c3c-a1c4-cc60f01af8c6>
2010-05-11 11:49:30 +0000

    [What] Add License GPLv3 info in SudokuBoxer

--- SudokuBoxer.py
+++ SudokuBoxer.py
@@ -1,3 +1,21 @@
+#
+#    SudokuBoxer is a Sudoku GUI game & solver
+#
+#    Copyright (C) 2010  Falldog
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 import wx
 import wx.lib.newevent
 from copy import deepcopy
@@ -11,6 +29,21 @@ from boxer import SudokuBoxer, Step
 from user import GetUserInfo
 _ = wx.GetTranslation
 
+
+LICENSE = '''
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 class Number:
     def __init__(self, val=0):
@@ -955,14 +988,14 @@ class MainFrame(wx.Frame):
         info = wx.AboutDialogInfo()
         info.Name = "Sudoku Boxer"
         info.Version = App.VERSION + '.' + App.VERSION_DATE
-        info.Copyright = "(C) 2010 Programmers and Coders Everywhere"
+        info.Copyright = "(C) 2010 Falldog"
         info.Description = wordwrap(
             "Sudoku Boxer, play and solve with sudoku software. Enjoy it!",
             350, wx.ClientDC(self))
         info.WebSite = ("http://falldog7.blogspot.com", "Falldog's blog")
         info.Developers = ["Falldog"]
         
-        #info.License = wordwrap(licenseText, 500, wx.ClientDC(self))
+        info.License = wordwrap(LICENSE, 500, wx.ClientDC(self))
 
         # Then we call wx.AboutBox giving it that info object
         wx.AboutBox(info)
