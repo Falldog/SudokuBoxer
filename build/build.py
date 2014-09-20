7158329e67ccddf270c2686b1a8d4a2d3e6c062b
Falldog <falldog7@gmail.com>
2014-09-21 07:39:42 +0800

    refactor, refine build script, remove build tools in Git, download it when build time

--- build/build.py
+++ build/build.py
@@ -15,7 +15,6 @@ CUR_DIR = abspath(dirname(__file__))
 ROOT_DIR = abspath(join(CUR_DIR, '..'))
 DIST_DIR = join(CUR_DIR, 'dist')
 PUZZLE_DIR = join(CUR_DIR, 'puzzle')
-GETTEXT_DIR = join(CUR_DIR, 'gettext')
 VERSION_PATH = join(ROOT_DIR, 'version')
 SRC_DIR = join(ROOT_DIR, 'src')
 LANG_DIR = join(ROOT_DIR, 'lang')
@@ -32,7 +31,7 @@ def parse_args():
 
 class Builder():
     def __init__(self):
-        self.ver_date = build_util.GenerateVersion(VERSION_PATH)
+        self.ver_date = build_util.generate_version(VERSION_PATH)
         self.dist_dir = join(DIST_DIR, 'SudokuBoxer-%s' % self.ver_date)
         self.puzzle_dir = join(self.dist_dir, 'puzzle')
         self.lang_dir = join(self.dist_dir, 'lang')
@@ -43,6 +42,10 @@ class Builder():
             shutil.rmtree(self.dist_dir)
         if os.path.isdir(join(DIST_DIR, 'SudokuBoxer')):
             shutil.rmtree(join(DIST_DIR, 'SudokuBoxer'))
+
+        self.gettext_dir = build_util.prepare_tool_gettext(CUR_DIR)
+        self.libiconv2_dir = build_util.prepare_tool_libiconv2(CUR_DIR)
+        self.pyinstaller_dir = build_util.prepare_tool_pyinstaller(CUR_DIR)
 
     def build(self):
         self.update_MUI_mo()
@@ -59,7 +62,8 @@ class Builder():
         self.generate_py_list(py_list_path, [ROOT_DIR, SRC_DIR], [xrc_string_path])
         env = os.environ.copy()
         if sys.platform == 'win32':
-            env['PATH'] = env['PATH'] + os.pathsep + GETTEXT_DIR
+            env['PATH'] += os.pathsep + self.gettext_dir
+            env['PATH'] += os.pathsep + self.libiconv2_dir
 
         subprocess.check_call('xgettext -o "%s" --files-from=%s' % (join(LANG_DIR, 'base.po'),
                                                                     py_list_path),
@@ -80,7 +84,8 @@ class Builder():
         msgmerge_output_flags = '--output-file'
         env = os.environ.copy()
         if sys.platform == 'win32':
-            env['PATH'] = env['PATH'] + os.pathsep + GETTEXT_DIR
+            env['PATH'] += os.pathsep + self.gettext_dir
+            env['PATH'] += os.pathsep + self.libiconv2_dir
         
         subprocess.check_call('msgfmt %s "%s" "%s"' % (msgmerge_output_flags,
                                                        get_lang_path(LANG_DIR, 'CHT', ext='.mo'),
@@ -115,7 +120,7 @@ class Builder():
         cwd = os.getcwd()
         os.chdir(CUR_DIR)
         try:
-            subprocess.check_call('python %s SudokuBoxer.spec' % join(CUR_DIR, 'pyinstaller-1.5.1', 'pyinstaller.py'), shell=True)
+            subprocess.check_call('python %s SudokuBoxer.spec' % join(self.pyinstaller_dir, 'pyinstaller.py'), shell=True)
 
             # rename to SudokuBoxer-[version]
             ori_dist_dir = join(DIST_DIR, 'SudokuBoxer')
@@ -133,6 +138,7 @@ class Builder():
 
         shutil.copytree(join(ROOT_DIR, 'img'), join(self.dist_dir, 'img'))
 
+        # copy lang
         self.lang_dir = join(self.dist_dir, 'lang')
         os.mkdir(self.lang_dir)
         for d in os.listdir(LANG_DIR):
@@ -141,6 +147,11 @@ class Builder():
             os.makedirs(join(self.lang_dir, d, 'LC_MESSAGES'))
             shutil.copyfile(join(LANG_DIR, d, 'LC_MESSAGES', 'default.mo'),
                             join(self.lang_dir, d, 'LC_MESSAGES', 'default.mo'))
+
+        # copy XRC resource
+        dist_xrc_dir = join(self.dist_dir, 'resource', 'xrc')
+        os.makedirs(dist_xrc_dir)
+        shutil.copyfile(join(XRC_DIR, 'PreferenceDialog.xrc'), join(dist_xrc_dir, 'PreferenceDialog.xrc'))
 
     def remove_redundant_files(self):
 
--- build/build_util.py
+++ build/build_util.py
@@ -1,13 +1,16 @@
 import os
 import sys
+import urllib2
 from os.path import join
+from zipfile import ZipFile
+
 
 def get_lang_path(basedir, lang, ext='.mo', domain='default'):
     lang_map = {'ENU': 'en_US', 'CHT': 'zh_TW'}
     return join(basedir, lang_map[lang], 'LC_MESSAGES', '%s%s' % (domain, ext))
 
 
-def GenerateVersion(version_path):
+def generate_version(version_path):
     import datetime
     f = open(version_path, 'r')
     lines = f.readlines()
@@ -22,4 +25,56 @@ def GenerateVersion(version_path):
     f.close()
 
     return '%s.%s' % (ver, ver_date)
+
+
+def download_zip_extract_to(url, extract_dir):
+    zip_tmp_file = join(extract_dir, 'tmp.zip')
+    if not os.path.exists(extract_dir):
+        os.makedirs(extract_dir)
+
+    response = urllib2.urlopen(url)
+    with open(zip_tmp_file, 'wb') as f:
+        f.write(response.read())
+
+    zip = ZipFile(zip_tmp_file)
+    zip.extractall(extract_dir)
+    zip.close()
+    os.remove(zip_tmp_file)
+
+
+def prepare_tool_libiconv2(CUR_DIR):
+    if sys.platform != 'win32':
+        return
+
+    print 'prepare - libiconv2'
+    url = 'https://sourceforge.net/projects/gnuwin32/files/libiconv/1.9.2-1/libiconv-1.9.2-1-bin.zip/download'
+    bin_dir = join(CUR_DIR, 'bin')
+    libiconv2_dir = join(bin_dir, 'libiconv-1.9.2-1')
+    libiconv2_bin_dir = join(libiconv2_dir, 'bin')
+    if not os.path.exists(libiconv2_dir):
+        download_zip_extract_to(url, libiconv2_dir)
+    return libiconv2_bin_dir
+
+
+def prepare_tool_gettext(CUR_DIR):
+    if sys.platform != 'win32':
+        return
+
+    print 'prepare - gettext'
+    url = 'https://sourceforge.net/projects/gnuwin32/files/gettext/0.14.4/gettext-0.14.4-bin.zip/download'
+    gettext_dir = join(CUR_DIR, 'bin', 'gettext-0.14.4')
+    gettext_bin_dir = join(gettext_dir, 'bin')
+    if not os.path.exists(gettext_dir):
+        download_zip_extract_to(url, gettext_dir)
+    return gettext_bin_dir
+
+
+def prepare_tool_pyinstaller(CUR_DIR):
+    print 'prepare - PyInstaller'
+    url = 'https://sourceforge.net/projects/pyinstaller/files/1.5/pyinstaller-1.5.1.zip/download'
+    bin_dir = join(CUR_DIR, 'bin')
+    pyinstaller_dir = join(bin_dir, 'pyinstaller-1.5.1')
+    if not os.path.exists(pyinstaller_dir):
+        download_zip_extract_to(url, bin_dir)
+    return pyinstaller_dir
 
